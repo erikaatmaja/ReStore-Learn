ae08fb576747e7c4ec52b9167a308d9530edb9d8
Erika Atmaja <atmajaerika@gmail.com>
2023-05-08 16:50:37 +0700

    End of section 5

---
+++ client/src/app/errors/ServerError.tsx
@@ -0,0 +1,24 @@
+import { Container, Divider, Paper, Typography } from "@mui/material";
+import { useLocation } from "react-router-dom";
+
+export default function ServerError(){
+    const {state} = useLocation();
+
+    return (
+        <Container component={Paper}>
+            {state?.error ? (
+                <>
+                    <Typography variant="h3" color="secondary" gutterBottom>
+                        {state.error.title}    
+                    </Typography> 
+                    <Divider />
+                    <Typography variant="body1">
+                        {state.error.detail || 'Internal Sever Error'}
+                    </Typography>
+                </>
+            ) : (            
+                <Typography variant="h5" gutterBottom>Server Error</Typography>
+            )}
+        </Container>
+    )
+}
--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,9 +1,38 @@
-import { Typography } from "@mui/material";
+import { Alert, AlertTitle, Button, ButtonGroup, Container, List, ListItem, ListItemText, Typography } from "@mui/material";
+import agent from "../../app/api/agent";
+import { useState } from "react";
 
 export default function AboutPage(){
+    const [validationErrors, setValidationErrors] = useState<string[]>([]);
+
+    function getValidationError() {
+        agent.TestErrors.getValidationError()
+             .then(() => console.log('should not see this'))
+             .catch(error => setValidationErrors(error));
+    }
+    
     return (
-        <Typography variant="h2">
-            About Page
-        </Typography>
+        <Container>
+            <Typography variant="h2">Errors for testing purposes</Typography>
+            <ButtonGroup fullWidth>
+                <Button variant="contained" onClick={() => agent.TestErrors.get400Error().catch(error => console.log(error))}>400 - Bad Request</Button>
+                <Button variant="contained" onClick={() => agent.TestErrors.get401Error().catch(error => console.log(error))}>401 - Unauthorized</Button>
+                <Button variant="contained" onClick={() => agent.TestErrors.get404Error().catch(error => console.log(error))}>404 - Not Found</Button>
+                <Button variant="contained" onClick={() => agent.TestErrors.get500Error().catch(error => console.log(error))}>500 - Server Error</Button>
+                <Button variant="contained" onClick={getValidationError}>Validation Error</Button>
+            </ButtonGroup>
+            {validationErrors.length > 0 &&
+                <Alert severity="error">
+                    <AlertTitle>Validation Errors</AlertTitle>
+                    <List>
+                        {validationErrors.map(error => (
+                            <ListItem key={error}>
+                                <ListItemText>{error}</ListItemText>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Alert>
+            }
+        </Container>
     )
 }
